4752f497dfe539764b1228d37f7f8fb44268604b
jt <285037039@qq.com>
2020-11-24 20:13:26 +0800

    test

--- docker/launch_docker.sh
+++ docker/launch_docker.sh
@@ -1,5 +1,5 @@
 # Thanks @dustinfreeman for providing the script
 #!/bin/bash
-sudo nvidia-docker build --build-arg HTTP_PROXY=192.168.0.107:8000 -t vid2vid:CUDA9-py35 ./docker/
+sudo nvidia-docker build -t vid2vid:CUDA9-py35 ./docker/
 
-sudo nvidia-docker run --rm -ti --ipc=host --shm-size 8G -v $(pwd):/vid2vid --workdir=/vid2vid vid2vid:CUDA9-py35 /bin/bash
+sudo nvidia-docker run --rm -ti --ipc=host --shm-size 8G -v $(pwd)/output:/output --workdir=/vid2vid vid2vid:CUDA9-py35 /bin/bash
