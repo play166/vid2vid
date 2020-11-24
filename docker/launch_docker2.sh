c067546f04fb54883693fd8f213c909d2b8550d9
jt <285037039@qq.com>
2020-11-24 20:05:12 +0800

    test

--- docker/launch_docker2.sh
+++ docker/launch_docker2.sh
@@ -2,7 +2,7 @@
 #!/bin/bash
 sudo nvidia-docker build --build-arg HTTP_PROXY=192.168.0.107:8000 -t vid2vid:CUDA9-py35 ./docker/
 
-sudo nvidia-docker run --rm -ti --ipc=host --shm-size 8G -v $(pwd):/vid2vid --workdir=/vid2vid play166/vid2vid:CUDA9-py35 /bin/bash
+sudo nvidia-docker run --rm -ti --ipc=host --shm-size 8G -v $(pwd)/output:/output --workdir=/vid2vid play166/vid2vid:CUDA9-py35 /bin/bash
 sudo docker run --gpus all --rm -ti --ipc=host --shm-size 8G -v $(pwd):/vid2vid --workdir=/vid2vid play166/vid2vid:CUDA9-py35 /bin/bash
 
 apt install libgl1-mesa-glx
